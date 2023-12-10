1bbc301423c85fefd90e7ae5656cd08dc2ec2e82
muhammad hamza <hamzaabdulmajeed863@gmail.com>
2023-12-10 23:44:54 +0500

    crud-13

--- routes/auth.mjs
+++ routes/auth.mjs
@@ -3,11 +3,11 @@ let router = express.Router()
 
 
 
-router.post('/login', (req, res, next) => {
+router.get('/login', (req, res, next) => {
     console.log('this is login!', new Date());
     res.send('this is login v1' + new Date());
 })
-router.post('/signup', (req, res, next) => {
+router.get('/signup', (req, res, next) => {
     console.log('this is signup!', new Date());
     res.send('this is signup v1' + new Date());
 })
